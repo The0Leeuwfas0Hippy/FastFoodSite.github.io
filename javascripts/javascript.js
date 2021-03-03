39bf80e3f9fb02c2108bc2d5cddd6beeeb68890e
Moifadi <moifadi15@gmai.com>
2021-03-03 06:49:38 -0800

    Initial commit

---
+++ javascripts/javascript.js
@@ -0,0 +1,80 @@
+
+/*
+    Product slider(s) on index page
+*/
+var index_ProductSlider = document.getElementsByClassName("EachProductOnSlider"),
+    index_Slider = document.getElementById("index_Products_Slider");
+
+//Menu buttons on index page
+var SeeFullMenu, ContactUs, MakeCustomOrder, 
+    index_LoginButton = document.getElementsByClassName("LoginLink"),
+    index_RegisterAccountButton = document.getElementsByClassName("RegistrationLink");
+
+//Login and registration forms on LoginRegistration.html
+var LoginForm = document.getElementsByClassName("Login"), 
+    RegistrationForm = document.getElementsByClassName("Registration");
+
+//MakeCustomOrder form
+index_ChooseCustomFood = document.getElementById("index_ChooseCustomFood");
+
+$(function()
+            {
+                // method for the Product Sliders to Dropdown sequentially one after another 
+
+            }
+ );
+
+ /*The code below handles the availing of the correct form(Login/Registration) 
+    based on which button(Login || Create Account) was pressed.  
+
+    -If login button is pressed: set 'login' button div class to active and registration form to be inactive
+            and if 'login' button div class is active, then set correct form to active
+            and set said form to be properly visible 
+ */
+index_LoginButton.onClick = function()
+{
+    $(function()
+                {
+                    //toggle the active class on both the login form and the registration form
+                    index_LoginButton.ClassList.toggle("active");
+
+                    $(LoginForm).css
+                                    ({
+                                        display: "flex"
+                                    });
+
+                    $(RegistrationForm).css
+                                    ({
+                                        display: "none"
+                                    });
+                }
+     );
+}
+
+index_RegisterAccountButton.onClick = function()
+{
+    $(function()
+                {
+                    $(RegistrationForm).css
+                                          ({
+                                              display: "flex"
+                                          });
+
+                            $(LoginForm).css
+                                          ({
+                                              display: "none"
+                                          });
+                                        
+                }
+     );
+}
+
+/*The Code below handles the Custom Order making
+*/
+index_ChooseCustomFood.onclick = function()
+{
+    $(function()
+    {
+         $(".CustomFoodItems").toggle(1000);
+    });
+}
